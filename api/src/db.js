1ad4700e39518096aa6ead49696b015530bf055c
Danielo64 <danyjogador64@gmail.com>
2021-09-21 18:32:53 -0300

    subiu uhuu

---
+++ api/src/db.js
@@ -0,0 +1,11 @@
+import initdb from './models/init-models.js'
+
+import Sequelize from 'sequelize';
+const sequelize = new Sequelize(
+    'mysql_17753_nsftcc',
+    'nsftcc',
+    'nsf@tcc', {
+    host: 'my01.winhost.com',
+    dialect: 'mysql',
+    logging: false
+});
