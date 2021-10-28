2e2b43ca0fd8ba0a4b6e712f2f0993f126dea4f7
Danielo64 <danyjogador64@gmail.com>
2021-10-28 15:25:57 -0300

    apiii

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,28 @@
 import db from './db.js';
 import express from 'express'
 import cors from 'cors'
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+app.get('/candidato', async (req, resp) => {
+    try {
+        
+    } catch {
 
+    }
+})
+
+app.post('/candidato', async (req, resp) => {
+    try {
+
+    } catch {
+
+    }
+})
+
+app.put('/candidato/:id')
 
 app.listen(process.env.PORT,
             x => console.log(`Server up at port ${process.env.PORT}`))
