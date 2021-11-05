f309faa8e20703a4ee0dae0ecf43b3b2dbf094f7
Danielo64 <danyjogador64@gmail.com>
2021-11-04 23:11:43 -0300

    aaaaaa

--- react/site/src/pages/tela inicial/index.js
+++ react/site/src/pages/tela inicial/index.js
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom'
 
 import { Container } from './styled'
 import CabecalhoVisitante from '../../components/cabecalho - visitante'
-import CabecalhoCandidato from '../../components/cabecalho - candidato'
-import CabecalhoEmpresa from '../../components/cabecalho - empresa'
 
 export default function Index() {
     return (
