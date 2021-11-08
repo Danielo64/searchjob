c18cec87d9b125200f7a9c79af02fbdbca31ea04
Danielo64 <danyjogador64@gmail.com>
2021-11-08 16:51:29 -0300

    api no react

--- react/site/src/services/api.js
+++ react/site/src/services/api.js
@@ -2,3 +2,21 @@ import axios from 'axios'
 const api = axios.create({
 baseURL: 'http://localhost:3030'
 })
+
+export default class Api {
+    // Candidato
+
+    async listarCandidato() {
+        let r = await api.get('/candidato');
+        return r.data;
+    }
+
+    async inserirCandidato() {
+        let r = await api.post('/candidato', { nome, email, senha, sexo, imagem, nacionalidade, telefone, nascimento, civil, localidade });
+        return r.data;
+    }
+
+    async removerCandidato(id) {
+        let r = await api.delete('/candidato' + id);
+    }
+}
